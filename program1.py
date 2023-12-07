9abc7734fbd9c24cc66a3f535962101c11cc2bb6
suren <easyexam@gmail.com>
2023-12-07 16:28:51 -0500

    v3

--- program1.py
+++ program1.py
@@ -6,10 +6,33 @@ print("4.Display the contents of the cart.")
 print("5.Purchase items.")
 print("6.Quit.")
 
-a=(input("product name"))
-b=int(input("price of the product"))
-c=(input("enter the brand name"))
- 
-Cart={}
-if Cart>=5:
-  print("cart is full")
+a={}
+x=(input("enter the product"))
+y=(input("enter the price"))
+z=(input("enter the brand name"))
+
+while(x!=6):
+       print("WELCOME TO THE AMANDO SHOPPING SITE")
+print("1.Add a product to the cart. ")
+print("2.Search for a product. ")
+print("3.Delete a product from the cart.")
+print("4.Display the contents of the cart.")
+print("5.Purchase items.")
+print("6.Quit.")
+       
+
+
+
+a=input("enter your choice")
+if a==1:
+    print("add product")
+    
+elif a==2:
+    print("search product")
+
+elif a==3:
+    print("delete the product")
+elif a==4:
+    print("display the product")
+elif a==5:
+    print("purchase items")
